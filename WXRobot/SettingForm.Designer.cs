d841fa967e8cfee27f29216c1ef09320d0506c4e
handong <986850427@qq.com>
2019-09-16 15:19:46 +0800

    ttt

--- WXRobot/SettingForm.Designer.cs
+++ WXRobot/SettingForm.Designer.cs
@@ -58,6 +58,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(344, 327);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
